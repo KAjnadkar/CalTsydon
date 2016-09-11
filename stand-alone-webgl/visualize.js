5f330b85f71075c98ce92bf1302b4d1946d321a4
KAjnadkar <KAjnadkar@gmail.com>
2016-09-10 22:45:29 -0500

    Upgrade to WebGl cube

--- stand-alone-webgl/visualize.js
+++ stand-alone-webgl/visualize.js
@@ -39,8 +39,8 @@ function play(){
 	bufferLength = analyser.frequencyBinCount;
 	dataArray = new Uint8Array(bufferLength);
 	
-	drawVisual = requestAnimationFrame(getFreq);
-	getFreq();
+	requestAnimationFrame(getFreq);
+//	getFreq();
 }
 
 function stop(){
@@ -57,6 +57,11 @@ function initWebgl(){
 	
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.enable(gl.DEPTH_TEST);
+	gl.enable(gl.CULL_FACE);
+	gl.frontFace(gl.CCW);
+	gl.cullFace(gl.BACK);
+	
 	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 	
@@ -83,38 +88,129 @@ function initWebgl(){
 		return;
 	}
 	
-	var vertices = [
-	                0.0, 0.5, 1.0, 0.0, 0.0, 
-	                -0.5, -0.5, 0.0, 1.0, 0.0,
-	                0.5, -0.5, 0.0, 0.0, 1.0
+	var boxVertices = [//x y x r g b
+	                -1.0, 1.0, -1.0, 0.0, 1.0, 1.0,
+	                -1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+	                1.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+	                1.0, 1.0, -1.0, 0.0, 1.0, 1.0,
+	                
+	                -1.0, 1.0, 1.0, 1.0, 0.0, 1.0,	                
+	                -1.0, -1.0, 1.0, 1.0, 0.0, 1.0,
+	                -1.0, -1.0, -1.0, 1.0, 0.0, 1.0,
+	                -1.0, 1.0, -1.0, 1.0, 0.0, 1.0,
+	                
+	                1.0, 1.0, 1.0, 1.0, 1.0, 1.0,	                
+	                1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+	                1.0, -1.0, -1.0, 1.0, 1.0, 1.0,
+	                1.0, 1.0, -1.0, 1.0, 1.0, 1.0,
+	                
+	                1.0, 1.0, 1.0, 0.0, 0.4, 0.4,	                
+	                1.0, -1.0, 1.0, 0.0, 0.4, 0.4,
+	                -1.0, -1.0, 1.0, 0.0, 0.4, 0.4,
+	                -1.0, 1.0, 1.0, 0.0, 0.4, 0.4,
+	                
+	                1.0, 1.0, -1.0, 0.0, 1.0, 0.0,	                
+	                1.0, -1.0, -1.0, 0.0, 1.0, 0.0,
+	                -1.0, -1.0, -1.0, 0.0, 1.0, 0.0,
+	                -1.0, 1.0, -1.0, 0.0, 1.0, 0.0,
+	                
+	                -1.0, -1.0, -1.0, 1.0, 0.0, 1.0,	                
+	                -1.0, -1.0, 1.0, 1.0, 0.0, 1.0,
+	                1.0, -1.0, 1.0, 1.0, 0.0, 1.0,
+	                1.0, -1.0, -1.0, 1.0, 0.0, 1.0
 	                ];
 	
-	var vartixBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vartixBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	var boxIndices = [
+	                  0, 1, 2,
+	                  0, 2, 3,
+	                  
+	                  5, 4, 6,
+	                  6, 4, 7,
+	                  
+	                  8, 9, 10,
+	                  8, 10, 11,
+	                  
+	                  13, 12, 14,
+	                  15, 14, 12,
+	                  
+	                  16, 17, 18,
+	                  16, 18, 19,
+	                  
+	                  21, 20, 22,
+	                  22, 20, 23
+	                  ];
+		
+	var vertixBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertixBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+	
+	var indexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 	
 	var positionAttributeLocation = gl.getAttribLocation(program, "vertPosition");
 	var colorAttributeLocation = gl.getAttribLocation(program, "vertColor");
-	gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
-	gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, gl.FALSE, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+	gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.vertexAttribPointer(colorAttributeLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
 	gl.enableVertexAttribArray(positionAttributeLocation);
 	gl.enableVertexAttribArray(colorAttributeLocation);
 	
+	var worldMatUniformLocation = gl.getUniformLocation(program, "worldMat");
+	var viewMatUniformLocation = gl.getUniformLocation(program, "viewMat");
+	var projMatUniformLocation = gl.getUniformLocation(program, "projMat");
+	
 	gl.useProgram(program);
-	gl.drawArrays(gl.TRIANGLES, 0, 3);
+	
+	var projMat = new Float32Array(16);
+	var worldMat = new Float32Array(16);
+	var viewMat = new Float32Array(16);
+	mat4.perspective(projMat, glMatrix.toRadian(45), canvas.width/canvas.height, 0.1, 1000.0);
+	mat4.identity(worldMat);
+	mat4.lookAt(viewMat, [0, 0, -8], [0, 0, 0], [0, 1, 0]);	
+	
+	gl.uniformMatrix4fv(worldMatUniformLocation, gl.FALSE, worldMat);
+	gl.uniformMatrix4fv(viewMatUniformLocation, gl.FALSE, viewMat);
+	gl.uniformMatrix4fv(projMatUniformLocation, gl.FALSE, projMat);	
+	
+	var angle = 0;
+	var identityMat = new Float32Array(16);
+	mat4.identity(identityMat);
+	
+	var xRotationMat = new Float32Array(16);
+	var zRotationMat = new Float32Array(16);
+	
+	var drawLoop = function() {		
+		angle = (performance.now() / 6000) * 2  * Math.PI;
+		
+		mat4.rotate(xRotationMat, identityMat, angle, [1, 0, 0]);
+		mat4.rotate(zRotationMat, identityMat, angle, [0, 0, 1]);
+		mat4.mul(worldMat, xRotationMat, zRotationMat);		
+		gl.uniformMatrix4fv(worldMatUniformLocation, gl.FALSE, worldMat);
+		
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);		
+		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+		
+		requestAnimationFrame(drawLoop);	
+	};
+	
+	requestAnimationFrame(drawLoop);
 	
 	console.log("web gl init end");
 }
 
 var vertexShaderText = [
                         "precision mediump float;",
-                        "attribute vec2 vertPosition;",
+                        "attribute vec3 vertPosition;",
                         "attribute vec3 vertColor;",
                         "varying vec3 fragColor;",
+                        "uniform mat4 worldMat;",
+                        "uniform mat4 viewMat;",
+                        "uniform mat4 projMat;",
                         "",
                         "void main(){",
                         "fragColor = vertColor;",
-                        "gl_Position = vec4(vertPosition, 0.0, 1.0);",
+                        "gl_Position = projMat * viewMat * worldMat * vec4(vertPosition, 1.0);",
                         "}"
                         ].join("\n");
 
